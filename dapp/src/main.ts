fa6a0ea4b15125e6414a40eed322117346de3687
Gabriel Barros <gabriel@1596.io>
2024-04-19 18:01:55 +0800

    Add boostrap code

---
+++ dapp/src/main.ts
@@ -0,0 +1,32 @@
+import CartesifyBackend from "@calindra/cartesify-backend";
+import { App } from "@deroll/app";
+import express from "express";
+
+let dapp:App
+CartesifyBackend.createDapp().then(initDapp => {
+    initDapp.start().then(() => {
+        console.log('Dapp initialized')
+    }).catch((e) => {
+        console.error(e);
+        process.exit(1);
+    });
+    dapp = initDapp
+})
+
+
+
+const app = express();
+const port = 8383;
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  req.header("x-msg_sender")
+  dapp.createNotice({payload:"0x1234"});
+  res.send({ some: "response" });
+
+});
+
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
