96e4fe712175a67c43e61036474a4cea81686df3
Marcus Souza <souza.mvsl@gmail.com>
2024-05-06 14:08:06 -0300

    feat: governance class fix

--- dapp/src/Governance.ts
+++ dapp/src/Governance.ts
@@ -3,77 +3,52 @@ import { Hex } from "viem";
 // Manages votes, decisions, validations of actions
 // used in the creation of new bet models
 // Can we leverage the DSG for DAOs?
-/**
- * The `Governance` class is responsible for managing votes, decisions, and validations of actions used in the creation of new bet models.
- * It provides functionality for adding and removing members from a list of addresses.
- */
 export default class Governance {
-  private static instance: Governance;
-  private members: Set<string>;
+    members: Array<string>
 
-  /**
-   * Private constructor to enforce singleton pattern.
-   * @param members An array of initial members.
-   */
-  private constructor(members: string[]) {
-    this.members = new Set(members);
-  }
-
-  /**
-   * Returns the singleton instance of the `Governance` class.
-   * @returns The singleton instance of the `Governance` class.
-   */
-  public static getInstance(): Governance {
-    if (!Governance.instance) {
-      Governance.instance = new Governance([
-        "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
-      ]);
+    private constructor(members: Array<string>) {
+        this.members = members;
     }
-    return Governance.instance;
-  }
+    private static instance:Governance;
 
-  /**
-   * Checks if the given address is a member of the governance.
-   * @param address The address to check.
-   * @returns True if the address is a member, false otherwise.
-   */
-  public isMember(address: string): boolean {
-    return this.members.has(address.toLowerCase());
-  }
-
-  /**
-   * Adds a new member to the governance if the `msgSender` is already a member.
-   * @param address The address of the new member.
-   * @param msgSender The address of the message sender.
-   * @throws Error if the `msgSender` is not a member or if the `address` is already a member.
-   */
-  public addMember(address: string, msgSender: string): void {
-    if (!this.isMember(msgSender.toLowerCase())) {
-      throw new Error("Only members can add new members");
+    public static getInstance(): Governance {
+        if (!Governance.instance) {
+            Governance.instance = new Governance(["0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"]);
+        }
+        return Governance.instance;
     }
 
-    if (this.isMember(address.toLowerCase())) {
-      throw new Error("This address is already a member");
+    isMember(address: string) {
+        return this.members.includes(address.toLowerCase());
     }
 
-    this.members.add(address.toLowerCase());
-  }
-
-  /**
-   * Removes a member from the governance if the `msgSender` is already a member.
-   * @param address The address of the member to remove.
-   * @param msgSender The address of the message sender.
-   * @throws Error if the `msgSender` is not a member or if the `address` is not a member.
-   */
-  public removeMember(address: string, msgSender: string): void {
-    if (!this.isMember(msgSender.toLowerCase())) {
-      throw new Error("Only members can remove members");
+    addMember(address: string, msgSender: string) {
+        address.toLowerCase()
+        if (this.isMember(msgSender.toLowerCase())) {
+            if (!this.isMember(address)){
+            this.members.push(address);
+            }
+            else{
+            throw new Error("This address is already a member");
+            }
+        } else {
+            throw new Error("Only members can add new members");
+        }
     }
 
-    if (!this.isMember(address.toLowerCase())) {
-      throw new Error("This address is not a member");
-    }
+    removeMember(address: string, msgSender: string): void {
+        address = address.toLowerCase();
+        msgSender = msgSender.toLowerCase();
 
-    this.members.delete(address.toLowerCase());
-  }
+        if (this.isMember(msgSender)) {
+            const index = this.members.indexOf(address);
+            if (index !== -1) {
+                this.members.splice(index, 1);
+            } else {
+                throw new Error("This address is not a member");
+            }
+        } else {
+            throw new Error("Only members can remove members");
+        }
+    }
 }
